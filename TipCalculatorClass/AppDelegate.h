9923059119b96fcbe4deae6bd98c8027eb96b32e
Ken Szubzda <kszubzda@box.com>
2015-01-07 08:14:32 -0800

    Initial Commit

---
+++ TipCalculatorClass/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TipCalculatorClass
+//
+//  Created by Ken Szubzda on 1/7/15.
+//  Copyright (c) 2015 Ken Szubzda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
